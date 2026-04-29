38b41b1f3d406a110a8c23ae487c44453d340cc2
raghavn-10 <raghavnatarajan18@gmail.com>
2026-04-29 11:14:27 +0530

    Day_3 Programming

---
+++ day_3/control_state.java
@@ -0,0 +1,19 @@
+
+
+class control_state{
+    public static void main(String[] args) {
+        int mark = 80;
+        if(mark>=91 && mark<=100)
+        System.out.println("O grade");
+        else if(mark>=81 && mark<=90)
+        System.out.println("A+ grade");
+        else if(mark>=71 && mark<=80)
+        System.out.println("A grade");
+        else if(mark>=61 && mark<=70)
+        System.out.println("B grade");
+        else if(mark>=51 && mark<=60)
+        System.out.println("C grade");
+        else
+        System.out.println("Next Sem Write");
+    }
+}
