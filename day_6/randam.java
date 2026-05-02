e23abdbd006c92481b32f77550f2572a44bce936
raghavn-10 <raghavnatarajan18@gmail.com>
2026-05-02 14:57:58 +0530

    Home Coming

--- day_6/randam.java
+++ day_6/randam.java
@@ -1,12 +1,17 @@
 
+import java.util.Arrays;
 import java.util.Random;
-
 class randam{
     public static void main(String[] args) {
         Random ra = new Random();
         int a = ra.nextInt(19);
         double gaus = ra.nextGaussian();
         boolean b = ra.nextBoolean();
-        System.out.println("Integer: "+a+"\n"+"Gaussian: "+gaus+"\n"+"Boolean Value: "+b);
+        double d = ra.nextDouble();
+        byte bt[] = new byte[5];
+        ra.nextBytes(bt);
+
+        System.out.println("Integer: "+a+"\n"+"Gaussian: "+gaus+"\n"+"Boolean Value: "+b+"\n"+"Double Value: "+d);
+        System.out.println("Bytes value: "+Arrays.toString(bt));
     }
 }
